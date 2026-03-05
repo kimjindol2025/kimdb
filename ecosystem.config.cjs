bc3e7daf50969fb60a9d9b6ea8ff8e557a6f4dcb
kim <kim@dclub.kr>
2026-03-06 00:05:39 +0900

    feat: KimDB 초기화

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,23 +2,20 @@ module.exports = {
   apps: [{
     name: 'kimdb',
     script: 'src/api-server.js',
-    cwd: '/home/kimjin/Desktop/kim/kimdb',
+    cwd: '/home/kimjin/kimdb',
+    interpreter: 'node',
     env: {
       PORT: 40000,
       NODE_ENV: 'production'
     },
-    // 로그 설정
     log_date_format: 'YYYY-MM-DD HH:mm:ss',
-    error_file: '/home/kimjin/Desktop/kim/kimdb/logs/error.log',
-    out_file: '/home/kimjin/Desktop/kim/kimdb/logs/out.log',
+    error_file: './logs/error.log',
+    out_file: './logs/out.log',
     merge_logs: true,
-    // 자동 재시작
     autorestart: true,
     max_restarts: 10,
     restart_delay: 3000,
-    // 메모리 초과 시 재시작
     max_memory_restart: '500M',
-    // 크래시 시 재시작
     exp_backoff_restart_delay: 1000
   }]
 };
